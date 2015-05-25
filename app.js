6096b17320d7a75fe54b5ed81dd9580d76f1d817
Damir Harambasic <damir.hara@gmail.com>
2015-05-25 14:03:28 -0600

    Added more logic and styles

--- app.js
+++ app.js
@@ -3,29 +3,36 @@ $(function () {
     	e.preventDefault();
     	var textfield = document.getElementById('textfield').value;
         var zipURLjson = 'http://api.openweathermap.org/data/2.5/weather?zip=' + textfield +',us&APPID=7c81a45ff51330c7cc25bc7f027131e8&units=imperial&callback=?';
-        if (textfield.length < 4) {
-	    		$('#error').text('* Please enter a 5-digit zip code.')
-	    		return;
+        if (textfield.length < 5) {
+	    		$('#error').text('* Please enter a 5-digit zip code.').delay(3000).fadeOut(500);
+	    		return false;
+	    }
+	    for (var i = 0; i <= 4; i++) {
+	    	var char = parseInt(textfield[i]);
+	    	if (isNaN(char)) {
+	    		$('#error').text('* Please enter only numbers.').delay(3000).fadeOut(500);
+	    		return false;
 	    	}
-
-	    $.getJSON(zipURLjson, function (data) {
+	    }
+	    
+	    $.getJSON(zipURLjson).done(function (data) {
 	    	var cityName = data.name,
 	    		weatherConditions = data.weather[0].description,
 	    		//iconConditions = data.weather[0].icon,
 	    		temperature = Math.round(data.main.temp);
 	    	weatherConditions.toLowerCase();
-
+	    	
 	    	$.each(data, function () {
 	    		$('#weather-result').text('Looks like the weather in ' +  cityName + ' consists of ' + weatherConditions + ' with the current temperature of ' + temperature + ' degrees.');
 	    	});
 	    	function zipresponse () {
-		        $('#input-result').text('You entered the zipcode ' + textfield + ', which is in ' + cityName + '.');
-		    };
-	        zipresponse(textfield);
-
-	    })
-	    .fail(function () {
-	    	$('#error').text('Sorry, something went wrong!');
+	    		$('#input-result').text('You entered the zipcode ' + textfield + ', which is in ' + cityName + '.');
+	    	};
+	    	
+	    	zipresponse(textfield);
+	    	
+	    }).fail(function () {
+	    	$('#weather-result').text('Sorry, something went wrong!');
 	    });
     	
 	    /*$.ajax({
