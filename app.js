9d95ccbec0b0dc7106ad5cfd720452769964e6f7
Damir Harambasic <damir.hara@gmail.com>
2015-05-31 21:37:38 -0600

    additional functionality and bug fixes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(function () {
     $('#subbtn').on('click', function (e) {
     	e.preventDefault();
     	var textfield = document.getElementById('textfield').value;
-        var zipURLjson = 'http://api.openweathermap.org/data/2.5/weather?zip=' + textfield +',us&APPID=7c81a45ff51330c7cc25bc7f027131e8&units=imperial&callback=?';
+        var zipURLjson = 'http://api.openweathermap.org/data/2.5/weather?zip=' + textfield + ',us&APPID=7c81a45ff51330c7cc25bc7f027131e8&units=imperial&callback=?';
         if (textfield.length < 5) {
 	    		$('#error').text('* Please enter a 5-digit zip code.').delay(3000).fadeOut(500);
 	    		return false;
@@ -15,26 +15,24 @@ $(function () {
 	    	}
 	    }
 	    
-	    $.getJSON(zipURLjson).done(function (data) {
-	    	var cityName = data.name,
-	    		weatherConditions = data.weather[0].description,
-	    		//iconConditions = data.weather[0].icon,
-	    		temperature = Math.round(data.main.temp);
-	    	weatherConditions.toLowerCase();
-	    	
-	    	$.each(data, function () {
-	    		$('#weather-result').text('Looks like the weather in ' +  cityName + ' consists of ' + weatherConditions + ' with the current temperature of ' + temperature + ' degrees.');
-	    	});
-	    	function zipresponse () {
-	    		$('#input-result').text('You entered the zipcode ' + textfield + ', which is in ' + cityName + '.');
-	    	};
-	    	
-	    	zipresponse(textfield);
-	    	
-	    }).fail(function () {
-	    	$('#weather-result').text('Sorry, something went wrong!');
-	    });
-    	
+		$.getJSON(zipURLjson).done(function (data) {
+		    var cityName = data.name,
+		    	weatherConditions = data.weather[0].description.toLowerCase(),
+		    	//iconConditions = data.weather[0].icon,
+		    	temperature = Math.round(data.main.temp);
+		    	
+		    $.each(data, function () {
+		    	$('#weather-result').text('Looks like the weather in ' +  cityName + ' consists of ' + weatherConditions + ' with the current temperature of ' + temperature + ' degrees.');
+		    });
+		    function zipresponse () {
+		    	$('#input-result').text('You entered the zipcode ' + textfield + ', which is in ' + cityName + '.');
+		    };
+		    	
+		    zipresponse(textfield);
+		    	
+		}).fail(function () {
+		    $('#weather-result').text('Sorry, something went wrong!');
+		});
 	    /*$.ajax({
 	    	url: zipURLjson,
 	    	dataType: 'jsonp',
@@ -57,6 +55,9 @@ $(function () {
 		    };
 	        zipresponse(textfield);
 	    */
+	    $('#location').on('click', function () {
+			navigator.geolocation.getCurrentPosition();
+		});
     });
-
+	
 });
